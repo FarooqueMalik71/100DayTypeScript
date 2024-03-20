968c871157e0ce7272f79b1d2b6fd34a3ed850d7
FarooqueMalik71 <64806753+FarooqueMalik71@users.noreply.github.com>
2024-03-20 17:15:36 +0500

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,13 @@
+//🚀 *Day 1 Challenge: Start Coding!* 🚀
+// *Question 1:* Install Node.js, TypeScript and VS Code on your computer/Laptop.
+// Done..
+console.log("Hello World");
+// *Question 2:* Personal Message: Store a person’s name in a variable and print a message to them, like “Hello //Farooque, would you like to learn some TypeScript today?”
+var name1 = "Farooque Malik";
+console.log("Hello ".concat(name1, " would you like to learn some TypeScript today? "));
+// *Question 3:* Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, //uppercase, and titlecase.
+var name2 = "faRooQue MaLik";
+console.log("your name\t", name2);
+console.log("to upper case\t", name2.toUpperCase());
+console.log("to lower case\t", name2.toLowerCase());
+console.log("to default case ", name2.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
