8b912e2051ed83c4d8800cff9287339737ebd8ad
FarooqueMalik71 <64806753+FarooqueMalik71@users.noreply.github.com>
2024-03-20 19:38:16 +0500

    Add files via upload

---
+++ DAY3/day3.js
@@ -0,0 +1,18 @@
+"use strict";
+//🚀 *Day 3 Challenge: Start Coding!* 🚀
+//*Question 7:* Number Eight: Write addition, subtraction, multiplication, and division operations that each result //in the number 10. Enclose your operations in print statements.
+console.log("sum 8 + 2 =", 8 + 2);
+console.log("sub  12 - 2 =", 12 - 2);
+console.log("multipli 5 * 2 =", 5 * 2);
+console.log("divide 20 / 2 =", 20 / 2);
+console.log("modulus 21 % 2 =", 21 % 2);
+//*Question 8:* You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(6 + 2);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//*Question 9:* Favorite Number: Store your favorite number in a variable. Then, create a message revealing your //favorite number. Print that message.
+let num1 = 7;
+console.log("My Favorit number is", `${num1}`);
